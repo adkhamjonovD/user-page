66994bb5675708ad7be3552c9e5375122640a8f9
adkhamjonovD <theadkhamjonov000@gmail.com>
2024-05-13 23:15:13 +0500

    refactor: firs commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./App.css";
+import Logo from "./assets/Logo.svg";
+import Calendar from "./assets/Calendar.svg";
+import Clock from "./assets/Clock.svg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="main__section">
+          <div className="section">
+            <div className="section_bg">
+              <div className="container">
+                <div className="section__box">
+                  <div className="logo">
+                    <img src={Logo} alt="" />
+                  </div>
+                  <div className="section__desc">
+                    <h1>
+                      "Yangi til o'rganishdagi <br /> eng mashhur bahonalar"
+                    </h1>
+                  </div>
+                  <div className="section__card">
+                    <div className="card__item">
+                      <img src={Calendar} alt="" />
+                      <h4>
+                        <span>KUNI:</span> <br /> 15-MAY
+                      </h4>
+                    </div>
+                    <div className="card__item">
+                      <img src={Clock} alt="" />
+                      <h4>
+                        <span>VAQTI:</span> <br /> 20:00
+                      </h4>
+                    </div>
+                  </div>
+                  <div className="section__regis">
+                    <button>hoziroq Ro‘yxatdan o‘ting</button>
+                  </div>
+                  <div className="section__num">
+                    <h2>01</h2>
+                    <p>
+                      Sardor Xudouberdiyevdan imtixonda qanday psixologik
+                      tayyorlaynish kerakligini bilib olasiz*
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
